33c9f0782e3553068b168f175fefb694b95a7013
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.brC:/Users/Pedro/AppData/Local/Programs/Python/Python38/python.exe>
2023-02-17 14:04:26 -0300

    add dist04

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,9 +7,9 @@ import { History } from './pages/History/History'
 export function Router() {
     return (
         <Routes>
-            <Route path='/' element={<DefaultLayout />} >
-                <Route path='/' element={<Home />} />
-                <Route path='history' element={<History />} />
+            <Route path='/02-ignite-timer/' element={<DefaultLayout />} >
+                <Route path='/02-ignite-timer/' element={<Home />} />
+                <Route path='/02-ignite-timer/history' element={<History />} />
             </Route>
         </Routes>
     )
